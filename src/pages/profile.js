958eab9747cd66b90fdd7ed7eec8d2d9935fcfac
Senadin <beganovicsenadin24@gmail.com>
2020-07-26 17:37:14 +0200

    finished profilename section in profile component

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,22 +1,167 @@
 import React from "react";
 import { useProfilePageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
-import { Hidden } from "@material-ui/core";
+import { Hidden, Card, CardContent, Button, Typography, Dialog, Avatar, Divider, Zoom } from "@material-ui/core";
 import { defaultCurrentUser } from "../data";
 import ProfilePicture from "../components/shared/ProfilePicture";
+import { Link } from "react-router-dom";
+import { GearIcon } from "../icons";
 
 function ProfilePage() {
   const classes = useProfilePageStyles();
+  const [showOptionsMenu, setOptionsMenu] = React.useState(false);
+
+  function handleOptionsMenuClick() {
+    setOptionsMenu(true);
+  }
+  function handleCloseMenu() {
+    setOptionsMenu(false);
+  }
 
   const isOwner = true;
   return (
-    <Layout hideFooter title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})`}>
+    <Layout
+      hideFooter
+      title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})`}
+    >
       <div className={classes.container}>
         <Hidden xsDown>
-          <ProfilePicture isOwner={isOwner} />
+          <Card className={classes.cardLarge}>
+            <ProfilePicture isOwner={isOwner} />
+            <CardContent className={classes.cardContentLarge}>
+              <ProfileNameSection
+                user={defaultCurrentUser}
+                isOwner={isOwner}
+                handleOptionsMenuClick={handleOptionsMenuClick}
+              />
+            </CardContent>
+          </Card>
         </Hidden>
       </div>
     </Layout>
+  );
+}
+
+function ProfileNameSection({ user, isOwner, handleOptionsMenuClick }) {
+  const classes = useProfilePageStyles();
+  const [showUnfollowDialog, setUnfollowDialog] = React.useState(false);
+
+  let followButton;
+  const isFollowing = true;
+  const isFollower = false;
+  if (isFollowing) {
+    followButton = (
+      <Button
+        onClick={() => setUnfollowDialog(true)}
+        variant="outlined"
+        className={classes.button}
+      >
+        Following
+      </Button>
+    );
+  } else if (isFollower) {
+    followButton = (
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Follow Back
+      </Button>
+    );
+  } else {
+    followButton = (
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Follow
+      </Button>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <Hidden xsDown>
+        <section className={classes.usernameSection}>
+          <Typography className={classes.username}>{user.username}</Typography>
+          {isOwner ? (
+            <React.Fragment>
+              <Link to="/accounts/edit">
+                <Button variant="outlined">Edit Profile</Button>
+              </Link>
+              <div
+                onClick={handleOptionsMenuClick}
+                className={classes.settingsWrapper}
+              >
+                <GearIcon className={classes.settings} />
+              </div>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>{followButton}</React.Fragment>
+          )}
+        </section>
+      </Hidden>
+      <Hidden smUp>
+        <section>
+          <div className={classes.usernameDivSmall}>
+            <Typography className={classes.username}>
+              {user.username}
+            </Typography>
+            {isOwner && (
+              <div
+                onClick={handleOptionsMenuClick}
+                className={classes.settingsWrapper}
+              >
+                <GearIcon className={classes.settings} />
+              </div>
+            )}
+          </div>
+          {isOwner ? (
+            <Link to="/accounts/edit">
+              <Button variant="outlined" style={{ width: "100%" }}>
+                Edit Profile
+              </Button>
+            </Link>
+          ) : (
+            followButton
+          )}
+        </section>
+      </Hidden>
+      {showUnfollowDialog && (
+        <UnfollowDialog user={user} onClose={() => setUnfollowDialog(false)} />
+      )}
+    </React.Fragment>
+  );
+}
+
+
+function UnfollowDialog({ onClose, user }) {
+  const classes = useProfilePageStyles();
+
+  return (
+    <Dialog
+      open
+      classes={{
+        scrollPaper: classes.unfollowDialogScrollPaper
+      }}
+      onClose
+      TransitionComponent={Zoom}
+    >
+      <div className={classes.wrapper}>
+        <Avatar
+          src={user.profile_image}
+          alt={`${user.username}'s avatar`}
+          className={classes.avatar}
+        />
+      </div>
+      <Typography
+        align="center"
+        variant="body2"
+        className={classes.unfollowDialogText}
+      >
+        Unfollow @{user.username}?
+      </Typography>
+      <Divider />
+      <Button className={classes.unfollowButton}>Unfollow</Button>
+      <Divider />
+      <Button onClick={onClose} className={classes.cancelButton}>
+        Cancel
+      </Button>
+    </Dialog>
   );
 }
 
